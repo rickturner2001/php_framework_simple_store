afed7fb46e2b702a3f9d1e942dc2a75ad19c4951
rickturner01 <rickturner2001@gmail.com>
2022-04-08 01:54:23 -0400

    updated composer

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php 
-echo "HEy there";
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+use app\controllers\ItemController;
+use app\core\Application;
+use app\controllers\SiteController;
+
+$app = new Application((__DIR__));
+
+$app->router->get("/", [SiteController::class, 'home']);
+$app->router->post("/", [ItemController::class, 'home']);
+// $app->router->get("/add_item", [SiteController::class, 'item']);
+$app->router->get("/add-product", [ItemController::class, 'item']);
+$app->router->post("/add-product", [ItemController::class, 'item']);
+
+
+$app->run();
